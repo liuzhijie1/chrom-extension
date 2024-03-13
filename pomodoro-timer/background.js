16677d0c687a52bcca087f70caa2bf4e4a17b81e
liujie <572890972@qq.com>
2024-03-13 19:47:05 +0800

    timer feature ok

--- pomodoro-timer/background.js
+++ pomodoro-timer/background.js
@@ -7,8 +7,18 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.storage.local.get(['timer', 'isRunning'], (res) => {
       if (res.isRunning) {
         let timer = res.timer + 1
+        let isRunning = true
+        if (timer === 60 * 25) {
+          this.ServiceWorkerRegistration.showNotification('Pomorodo Timer', {
+            body: '25 minutes has passed!',
+            icon: 'icon.png'
+          })
+          timer = 0
+          isRunning = false
+        }
         chrome.storage.local.set({
-          timer
+          timer,
+          isRunning
         })
       }
     })
